91117433f995a606acde01a3cb4efd76c9921e4d
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-04 18:41:47 +0100

    Проверка ДЗ | 3. Переменные и объекты
    - Правильно: №0, №3, №5, №6, №7, №10
    - Неправильно: №1, №2, №4, №8, №9
    - Улучшены: №0, №10
    - На доработку: №1, №2, №4, №7, №8, №9
    
    В целом все хорошо, исправьте ошибки!

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,7 +1,9 @@
-radius = float(input("Введите радиус круга:"))
+radius = float(input("Введите радиус круга: "))
 
 # S = pi * r^2
 pi = 3.14159
 area = pi * radius ** 2
 
 print("Площадь круга равна ", area)
+
+# Отлично!
