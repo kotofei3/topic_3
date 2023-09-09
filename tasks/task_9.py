bd847bb24df4380ac83b5efa35562642d348ffc8
Konstantin Kotov <konstantin.kotov1993@mail.ru>
2023-09-09 13:00:45 +0300

    DZ - 3. Переменные и объекты требует поправи 2 задачи,и не решена 9

--- tasks/task_9.py
+++ tasks/task_9.py
@@ -1,7 +1,5 @@
-import math
+number = float(input('Введите число: '))
 
-number = int(input('Введите число: '))
+square_root = number ** 0.5
 
-square_root = 3 ** 3
-
-print("Квадратный корень числа", number, "равен", float(math.ceil(square_root)))# и беда
+print("Квадратный корень числа", number, "равен", square_root)
